62004ca6a43c0566dae0e258e93f7c06318ce562
JimOfJam <160068904+JimOfJam@users.noreply.github.com>
2024-04-25 13:14:30 +0930

    Added interaction term

--- Gradient Descent Hubbard Hilbert.py
+++ Gradient Descent Hubbard Hilbert.py
@@ -1,38 +1,39 @@
-
 import gaussian as g
 import numpy as np
 import timeit 
 import matplotlib.pyplot as plt 
-import time
+import scipy.sparse
 
 j = 0+1j 
 
 # This function will help keep our indices notationally consistent with the Wikipedia page for the Hubbard model
 def index(i,sigma):
-    return i%4+sigma*4
-
-n = 10
+    return int(i%(n/2)+sigma*(n/2))
+n = 8
+t = 1
+U = 1
 
 # Initialising the Hilbert space
 space = g.FermionicSpace(n)
 
 # Defining the Hamiltonian 
-space.hamiltonian = np.zeros((space.dimension,space.dimension),dtype="complex_")
+space.hamiltonian = scipy.sparse.csc_array(([],([],[])),shape=(space.dimension,space.dimension),dtype="complex_")
 for i in range(int(n/2)):
     for sigma in range(2):
-        firstterm = np.matmul(space.raising[index(i,sigma)],space.lowering[index(i+1,sigma)])
-        space.hamiltonian -= firstterm + g.adjoint(firstterm)
+        firstterm = space.raising[index(i,sigma)] @ space.lowering[index(i+1,sigma)]
+        space.hamiltonian += -t*(firstterm + g.adjoint(firstterm))
+    space.hamiltonian += U * space.raising[index(i,0)] @ space.lowering[index(i,0)] @ space.raising[index(i,1)] @ space.lowering[index(i,1)]
 
 # This is the function to be minimised 
 expectationValue = lambda x : g.prodN([g.adjoint(x),space.hamiltonian,x])
 
-# Finds the real ground state by computing eigenvalues directly
+"""# Finds the real ground state by computing eigenvalues directly
 eig = np.linalg.eig(space.hamiltonian)
 min = 0
 for i in range(len(eig[0])):
     if eig[0][i].real < eig[0][min].real:
         min = i
-print("Real ground state energy:",eig[0][min].real)
+print("Real ground state energy:",eig[0][min].real)"""
 
 # Define the basis vectors of the Hilbert space
 basis = []
@@ -44,7 +45,7 @@ for i in range(space.dimension):
 # Perform the method of steepest descent
 psi = space.randomState()
 energy = expectationValue(psi).real
-epsilon = 0.1
+epsilon = 1
 
 energies = []
 starttime = timeit.default_timer()
@@ -63,12 +64,13 @@ for i in range(1000):
             break
     
     if i % 10 == 0:
-        print("Progress:" + str(i/10) + "%",end="\r") 
+        print("Progress: " + str(i/30*100) + "%, Current epsilon: " + str(epsilon) + ", Current energy: " + str(energy),end="\r") 
     energies.append(energy)
 
 stoptime = timeit.default_timer()
 print("Method terminated in",stoptime-starttime,"seconds with final epsilon value",str(epsilon)+".")
 print("Ground state energy:",energy)
+print("Ground state covariance matrix:\n",space.covariance(psi).real)
 
 plt.plot(range(i+1),energies)
 plt.show()
